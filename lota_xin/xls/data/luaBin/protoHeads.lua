3c35a6b98f4311312e2c3b25195b9836ba73a91c
yejunwei <657249950@qq.com>
2016-12-06 09:31:08 +0800

    qqq

--- lota_xin/xls/data/luaBin/protoHeads.lua
+++ lota_xin/xls/data/luaBin/protoHeads.lua
@@ -3,9 +3,9 @@ bossTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		ID 0 : *integer
+		ID 0 : integer
 		Name 1 : string
-		blood 2 : *integer
+		blood 2 : integer
 		Src 3 : string
 	}
 
@@ -20,11 +20,11 @@ equipTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		ID 0 : *integer
+		ID 0 : integer
 		Name 1 : string
 		Desc 2 : string
-		Type 3 : *integer
-		quality 4 : *integer
+		Type 3 : integer
+		quality 4 : integer
 		iconSrc 5 : string
 	}
 
@@ -39,37 +39,37 @@ speedTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		LV 0 : *integer
-		Attack01 1 : *integer
-		Attack02 2 : *integer
-		Attack03 3 : *integer
-		Attack04 4 : *integer
+		LV 0 : integer
+		Attack01 1 : integer
+		Attack02 2 : integer
+		Attack03 3 : integer
+		Attack04 4 : integer
 		Attack05 5 : *integer
-		Attack06 6 : *integer
-		Attack07 7 : *integer
-		Attack08 8 : *integer
-		Attack09 9 : *integer
-		Attack10 10 : *integer
-		Crit01 11 : *integer
-		Crit02 12 : *integer
-		Crit03 13 : *integer
-		Crit04 14 : *integer
-		Crit05 15 : *integer
-		Crit06 16 : *integer
-		Crit07 17 : *integer
-		Crit08 18 : *integer
-		Crit09 19 : *integer
-		Crit10 20 : *integer
-		Speed01 21 : *integer
-		Speed02 22 : *integer
-		Speed03 23 : *integer
-		Speed04 24 : *integer
-		Speed05 25 : *integer
-		Speed06 26 : *integer
-		Speed07 27 : *integer
-		Speed08 28 : *integer
-		Speed09 29 : *integer
-		Speed10 30 : *integer
+		Attack06 6 : integer
+		Attack07 7 : integer
+		Attack08 8 : integer
+		Attack09 9 : integer
+		Attack10 10 : integer
+		Crit01 11 : integer
+		Crit02 12 : integer
+		Crit03 13 : integer
+		Crit04 14 : integer
+		Crit05 15 : integer
+		Crit06 16 : integer
+		Crit07 17 : integer
+		Crit08 18 : integer
+		Crit09 19 : integer
+		Crit10 20 : integer
+		Speed01 21 : integer
+		Speed02 22 : integer
+		Speed03 23 : integer
+		Speed04 24 : integer
+		Speed05 25 : integer
+		Speed06 26 : integer
+		Speed07 27 : integer
+		Speed08 28 : integer
+		Speed09 29 : integer
+		Speed10 30 : integer
 	}
 
 	.speedTableBook {
@@ -83,11 +83,11 @@ goldTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		ID 0 : *integer
-		PlayerUpLv 1 : *integer
-		PlayerUpSkill 2 : *integer
-		PetUpLv 3 : *integer
-		PetUpSkill 4 : *integer
+		ID 0 : integer
+		PlayerUpLv 1 : integer
+		PlayerUpSkill 2 : integer
+		PetUpLv 3 : integer
+		PetUpSkill 4 : integer
 	}
 
 	.goldTableBook {
@@ -101,10 +101,10 @@ materialTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		ID 0 : *integer
+		ID 0 : integer
 		Name 1 : string
 		Desc 2 : string
-		quality 3 : *integer
+		quality 3 : integer
 		iconSrc 4 : string
 	}
 
@@ -119,13 +119,13 @@ petTableRes = {
 	protoHead	=
 	[[
 	.Unit {
-		ID 0 : *integer
+		ID 0 : integer
 		Name 1 : string
 		Pet_Desc 2 : string
 		quality 3 : *integer
 		Src 4 : string
 		headSrc 5 : string
-		materialID 6 : *integer
+		materialID 6 : integer
 		callNum 7 : integer
 		promotionNum 8 : integer
 	}
